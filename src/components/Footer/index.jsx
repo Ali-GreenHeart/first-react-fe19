3a8b77db1b2a8bf2cb2703323311e114da6e8e2c
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-10-12 12:09:15 +0400

    using of css

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import "./footer.css"
+
+const Footer = () => {
+    return (
+        <footer>
+            <h1 className="footer-heading">Ali</h1>
+            <h1 className="footer-heading">JED</h1>
+            <h1 className="footer-heading">FE-19</h1>
+        </footer>
+    )
+}
+export default Footer;
