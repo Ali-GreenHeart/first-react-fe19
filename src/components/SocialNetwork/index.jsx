928b5f9f226d379cf4cbae6b1e9e728b347a1d81
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-10-19 10:53:56 +0400

    add create post functionality

--- src/components/SocialNetwork/index.jsx
+++ src/components/SocialNetwork/index.jsx
@@ -6,21 +6,49 @@ import axios from "axios"
 
 const _url = 'https://6530c2906c756603295eff06.mockapi.io/posts'
 
+const getPosts = (_setPosts) => {
+    axios.get(_url)
+        .then(({ data }) => {
+            _setPosts(data)
+        })
+}
+
 const SocialNetwork = () => {
     const [posts, setPosts] = useState([])
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+
     useEffect(() => {
-        axios.get(_url)
-            .then(({ data }) => {
-                setPosts(data)
-            })
+        getPosts(setPosts)
     }, [])
 
+    const handleSubmit = () => {
+        if (title.length < 4 || description.length < 6) {
+            alert('title-i ve description-u dogru yazin! En azi 3 simvolluq title olsun, 5 simvolluq description')
+            return;
+        }
+        axios.post(_url, { title: title, description })
+            .then(() => {
+                getPosts(setPosts)
+            })
+        setTitle('')
+        setDescription('')
+    }
     return (
         <>
             <div className={styles.inputContainer}>
-                <Input placeholder="enter post's title" />
-                <TextArea placeholder="enter post's description" />
-                <Button title="POST IT!" />
+                <Input
+                    onEnter={handleSubmit}
+                    value={title}
+                    onChange={setTitle}
+                    placeholder="enter post's title" />
+                <TextArea
+                    onEnter={handleSubmit}
+
+                    value={description}
+                    onChange={setDescription}
+                    placeholder="enter post's description" />
+                <Button onClick={handleSubmit} title="POST IT!" />
             </div>
             <div className={styles.postsContainer}>
                 {
