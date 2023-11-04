c9d36dde927993c94423a7ca1fc972172cfddd00
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-11-04 09:46:00 +0400

    create first rootReducer and redux store

--- src/main.jsx
+++ src/main.jsx
@@ -7,19 +7,26 @@ import ModeContextProvider from './context-api/ModeContext'
 import UserContextProvider from './context-api/UsersContext'
 import "./index.css"
 import ManyModeContextProvider from './context-api/ManyModeContext'
-
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './redux'
 const reactRootElement = ReactDOM.createRoot(document.getElementById('root'))
+
+const store = createStore(rootReducer)
+// 1. obyektdir, immutable olmalidir!
 
 reactRootElement.render(
     <AuthContextProvider>
         {/* <ModeContextProvider> */}
-        <ManyModeContextProvider>
-            <UserContextProvider>
-                <BrowserRouter>
-                    <App />
-                </BrowserRouter>
-            </UserContextProvider>
-        </ManyModeContextProvider>
+        <Provider store={store}>
+            <ManyModeContextProvider>
+                <UserContextProvider>
+                    <BrowserRouter>
+                        <App />
+                    </BrowserRouter>
+                </UserContextProvider>
+            </ManyModeContextProvider>
+        </Provider>
         {/* </ModeContextProvider> */}
     </AuthContextProvider>
 )
---
+++ src/redux/index.jsx
@@ -0,0 +1,13 @@
+
+const initialStore = {
+    count: 0,
+    users: []
+}
+
+const rootReducer = (store = initialStore, action) => {
+    switch (action.type) {
+
+    }
+}
+
+export default rootReducer;
