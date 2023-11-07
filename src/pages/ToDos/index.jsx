3515a4dad5c159cd92cfcf1b7437169c36620dfe
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-11-07 10:39:45 +0400

    write get todos with rtk

--- src/pages/ToDos/index.jsx
+++ src/pages/ToDos/index.jsx
@@ -1,17 +1,31 @@
 import axios from "axios"
 import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import todosSlice from "../../redux-toolkit/slices/todosSlice"
 
 const url = 'https://dummyjson.com/todos'
 
-const ToDos = ({ }) => {
+const ToDos = () => {
+    const { actions } = todosSlice
+    const todos = useSelector((store) => store.todos.value)
+    const dispatch = useDispatch()
     useEffect(() => {
         axios.get(url).then(({ data }) => {
-            console.log(data.todos)
+            dispatch(actions.getTodos(data.todos))
         })
     }, [])
     return (
         <>
             <h1 style={{ textAlign: 'center' }}>Welcome to todos page</h1>
+            {
+                todos.map((todo) => {
+                    return (
+                        <p key={todo.id}>
+                            {todo.todo}
+                        </p>
+                    )
+                })
+            }
         </>
     )
 }
