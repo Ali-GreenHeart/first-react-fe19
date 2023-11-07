9dc9ffc5c963524b38d18b83c04095e0ae94d8c9
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-11-07 10:58:57 +0400

    add toggle-done reducer

--- src/redux-toolkit/slices/todosSlice.js
+++ src/redux-toolkit/slices/todosSlice.js
@@ -8,6 +8,14 @@ const todosSlice = createSlice({
     reducers: {
         getTodos: (store, { payload }) => {
             store.value = payload
+        },
+        toggleDone: (store, { payload }) => {
+            store.value = store.value.map((todo) => {
+                if (todo.id === payload) {
+                    return { ...todo, completed: !todo.completed }
+                }
+                return todo;
+            })
         }
     }
 })
