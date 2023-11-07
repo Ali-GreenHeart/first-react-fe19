059b9e2004f2dee627ee57ebd717348f949860df
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-11-07 10:39:36 +0400

    add todoslice to store

--- src/redux-toolkit/store.js
+++ src/redux-toolkit/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./slices/counterSlice";
+import todosSlice from "./slices/todosSlice";
 
 const store = configureStore({
     reducer: {
-        counter: counterSlice.reducer
+        counter: counterSlice.reducer,
+        todos: todosSlice.reducer,
     }
 })
 
